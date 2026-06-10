f34cbc792250afdc37659894e477b0d7a6e11856
webdesigntrident <webdesigntrident@gmail.com>
2026-06-10 12:42:37 +0900

    ポケモンAPIとAjax
    0610

--- 0527/28_vote_new/src/main.js
+++ 0527/28_vote_new/src/main.js
@@ -1,6 +1,16 @@
 // main.js
-import { animate } from "motion";
+import { inView, animate, stagger } from "motion";
 import { vote, getRates } from "./candidates.js";
+
+// スクロールで対象のオブジェクトが表示されたら
+
+inView(".card", () => {
+  animate(
+    ".card",
+    { opacity: [0, 1], y: [40, 0] },
+    { duration: 0.5, delay: stagger(1) },
+  );
+});
 
 // 全カードのバーを得票率に合わせてアニメーションさせる
 const animateBars = () => {
---
+++ 0610/pokemon_app/src/api.js
@@ -0,0 +1,7 @@
+export const getPokemon = async (name, signal) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+    signal,
+  });
+  if (!res.ok) throw new Error(`HTTPエラー: ${res.status}`);
+  return res.json();
+};
